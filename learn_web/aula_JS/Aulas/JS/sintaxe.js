1978a2cc24c9976491394e63096d0cca305e6900
Xande <vitorgarciavaz@usp.br>
2024-08-25 00:26:50 -0300

    atualizar 24-08-24

---
+++ learn_web/aula_JS/Aulas/JS/sintaxe.js
@@ -0,0 +1,11 @@
+// Demonstração de concatenação de variáveis no JS
+let a,b,c;
+a = "hello";
+b = "world";
+c = a + " " + b + "!";
+document.getElementById("result").innerHTML = "O resultado de <b>c</b> é: " + c ;
+
+// Demonstração  de funçÇAR em JS
+function hello(){
+    alert("Hello World!");
+}
