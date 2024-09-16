33aa6cb52ef11c2c2c034216644cb43e028c87f1
Xande <vitorgarciavaz@usp.br>
2024-09-16 15:55:51 -0300

    update exs 3 lista assembly

--- Org_arq/listas/lista_exercicios_Assembly/ex3.asm
+++ Org_arq/listas/lista_exercicios_Assembly/ex3.asm
@@ -1,6 +1,5 @@
 
 .text
-.align 2
 .globl main
 
 main:
@@ -15,11 +14,53 @@ main:
     li a7, 8                        # lê string que é colocada no espaço já alocado
     mv a1, s1                       # o tamanho da string deve contar o NULL
     ecall
+    mv s2, a0                       # s2 recebe endereço da string
 
     li a7, 4                        # print da string
     ecall
+    
+    mv a0, s2
+    mv a1, s1
+    jal invString
 
     li a7, 10             
-    ecall                           # fim de programa       
+    ecall                           # fim de programa
+
+
+invString:
+    li a2, 0                         # int i = 0
+    addi a3, a1, -1                  # max = número de caracteres na string s/ \0
+    sub sp, sp, a3                   # aloca espaço na pilha para guardar caracteres invertidos
+loopGuarda:
+    bge a2, a3 , cont                # continua loop se i < max
+    add t1, a0, a2
+    sub a4, a3, a2
+    addi a4, a4, -1
+    add t2, sp, a4
+    lb t3, 0(t1)
+    sb t3, 0(t2)
+    addi a2, a2, 1
+    j loopGuarda
+cont:
+    li a2, 0
+loopRecupera:
+    beq a2, a3, fim_invString
+    add t1, a0, a2
+    add t2, sp, a2
+    lb t3, 0(t2)
+    sb t3, 0(t1)
+    addi a2, a2, 1
+    j loopRecupera
+fim_invString:
+    add sp, sp, a3
+    li a7, 4
+    ecall
+    ret
+    
+    
+
+
+
+
 
 
